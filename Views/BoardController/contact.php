b3c7b356162ae62479aabb0ee866ae9f9959598c
tameuszzz <mat.dusik@gmail.com>
2020-04-23 19:23:34 +0200

    add new pages

---
+++ Views/BoardController/contact.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- header -->
+<?php include(dirname(__DIR__).'/Common/header.php'); ?>
+<!-- /header -->
+<title>Fooduro - Contact</title>
+<?php   if ($_SESSION) {
+            echo "<link rel='stylesheet' href='Public/css/navbar2.css'>";
+        }
+?>
+<link rel="stylesheet" href="Public/css/about.css">
+</head>
+<body>
+
+<!-- navbar -->
+<?php   if ($_SESSION) {
+            include(dirname(__DIR__).'/Common/navbar2.php');
+        } else {
+            include(dirname(__DIR__).'/Common/navbar1.php');
+        }
+?>
+<!-- /navbar -->
+
+<div class="main">
+    <h1>CONTACT PAGE</h1>
+    
+    
+</div>
+
+<!-- footer -->
+<?php include(dirname(__DIR__).'/Common/footer.php'); ?>
+<!-- /footer -->
