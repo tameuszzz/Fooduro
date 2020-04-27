05779a7ba756e5dc7a5d2fecb8644de5581b28e9
banakonda <50657302+banakonda@users.noreply.github.com>
2020-04-27 19:35:12 +0200

    Add files via upload

--- Repository/HelpRepository.php
+++ Repository/HelpRepository.php
@@ -2,16 +2,31 @@
 
 require_once "Repository.php";
 
-class UserRepository extends Repository {
-    public function sendMessage(string $content, int $ID_user, int $anwser){
+class HelpRepository extends Repository {
+    public function sendMessage(string $contents, int $ID_user, int $anwser){
         $stmt = $this->database->connect()->prepare('
-            INSERT INTO  HELP (content, ID_user, anwser) 
-            VALUES (:content, :ID_user, :anwser)
+            INSERT INTO  HELP (contents, ID_user, anwser) 
+            VALUES (:contents, :ID_user, :anwser)
             ');
-        $stmt->bindParam(':content', $content, PDO::PARAM_INT);
+        $stmt->bindParam(':contents', $contents, PDO::PARAM_STR);
         $stmt->bindParam(':ID_user', $ID_user, PDO::PARAM_INT);
         $stmt->bindParam(':anwser', $anwser, PDO::PARAM_INT);
         $stmt->execute();
     }  
+
+    public function allUserMessage(int $id){
+        $stmt = $this->database->connect()->prepare('
+        SELECT COUNT(ID_help) FROM HELP
+        WHERE ID_user = :id
+        AND anwser like 0
+        ');
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $nr = $stmt->fetch(PDO::FETCH_ASSOC);
+        foreach($nr as $one){
+            return $one;
+        }
+    }
 
 }
